7c285e97432363535e12e459ffe52f5812384f75
takeuchi kazuto <takeuck+github@gmail.com>
2023-01-17 19:37:36 +0900

    stop problematic minifying

--- gesture-frame.js
+++ gesture-frame.js
@@ -1,1 +1,363 @@
-const g=(r,t,e)=>r<t?t:r>e?e:r,u=r=>r<0?0:r,y=(r,t)=>{let e=0;for(let s=0;s<r.length;s++)e+=t(r[s]);return e},d=(r,t)=>r.length===1?t(r[0]):y(r,t)/r.length,m=r=>{let t;const e=()=>(t=void 0,r());return[()=>t??=requestAnimationFrame(e),()=>t!==void 0&&(cancelAnimationFrame(t),t=void 0)]};class E extends HTMLElement{static observedAttributes=["scale","min-scale","max-scale","offset-x","offset-y"];#i=1;get scale(){return this.#i}set scale(t){t=g(t,this.#t,this.#e),this.#i!==t&&(this.#o("scale",this.#i=t),this.#r.style.transform=`scale(${t})`)}#t=.1;get minScale(){return this.#t}set minScale(t){this.#t!==t&&t>0&&(this.#o("min-scale",this.#t=t),this.#i<t&&(this.scale=t))}#e=100;get maxScale(){return this.#e}set maxScale(t){this.#e!==t&&t>0&&(this.#o("max-scale",this.#e=t),this.#i>t&&(this.scale=t))}#n=0;get offsetX(){return this.#n}set offsetX(t){this.setOffset(t,this.#s)}#s=0;get offsetY(){return this.#s}set offsetY(t){this.setOffset(this.#n,t)}#c=0;#f=0;#h;#a;#r;#d;constructor(){super();const t=this.attachShadow({mode:"open"});t.innerHTML="<div part=container><div part=top-left></div><slot part=content></slot></div>",this.#h=t.firstElementChild,this.#a=this.#h.firstElementChild,this.#r=this.#a.nextElementSibling;{let e=1;const[s]=m(()=>e=1);this.#d=()=>{s(),e=0},this.addEventListener("scroll",()=>{if(e){const i=this.#p()[0].transformPoint({x:this.#c-this.scrollLeft,y:this.#f-this.scrollTop});this.#o("offset-x",this.#n=i.x),this.#o("offset-y",this.#s=i.y)}})}}#l=1;#o(t,e){this.#l=0,this.setAttribute(t,e),this.#l=1}attributeChangedCallback(t,e,s){this.#l&&e!==s&&(this[t.replace(/-([a-z])/g,(i,o)=>o.toUpperCase())]=+s)}connectedCallback(){this.#o("scale",this.#i),this.#o("min-scale",this.#t),this.#o("max-scale",this.#e)}#u="";#m=[new DOMMatrixReadOnly,new DOMMatrixReadOnly];#p(){let t="";for(let e=this;e;e=e.parentElement){const s=getComputedStyle(e).transform;s&&s!=="none"&&(t=`${s} ${t}`)}if(this.#u!==t){const e=new DOMMatrix(t);e.e=e.f=0,this.#u=t,this.#m=[DOMMatrixReadOnly.fromMatrix(e),DOMMatrixReadOnly.fromMatrix(e.inverse())]}return this.#m}setOffset(t,e){if(this.#n===t&&this.#s===e)return;this.#o("offset-x",this.#n=t),this.#o("offset-y",this.#s=e);const{x:s,y:i}=this.#p()[1].transformPoint({x:t,y:e}),o=this.#h.style;o.margin=`${this.#f=u(i)}px 0 0 ${this.#c=u(s)}px`,o.width=`${s<0?this.clientWidth-s:0}px`,o.height=`${i<0?this.clientHeight-i:0}px`,this.#d(),this.scrollTo(u(-s),u(-i))}_zoom(t,e,s){const i=this.#i,o=g(i*t,this.#t,this.#e);if(o!==i){const h=o/i-1,a=this.#a.getBoundingClientRect();this.scale=o,this.setOffset(this.#n+h*(a.x-e),this.#s+h*(a.y-s))}}zoom(t,e){const s=this.getBoundingClientRect(),i=e?.x,o=e?.y;this._zoom(t,s.x+(i===void 0?s.width*.5:typeof i=="number"?i:s.width*parseFloat(i)*.01),s.y+(o===void 0?s.height*.5:typeof o=="number"?o:s.height*parseFloat(o)*.01))}fit(t){const{offsetWidth:e,offsetHeight:s}=this,{offsetWidth:i,offsetHeight:o}=this.#r,h=t?.marginX??0,a=t?.marginY??0,c=(e-h-h)/i,n=(s-a-a)/o;c<n?(this.scale=c,this.setOffset(h,Math.floor(s-o*c)/2)):(this.scale=n,this.setOffset(Math.floor(e-i*n)/2,a))}fitX(t){const e=t?.margin??0;this.scale=(this.offsetWidth-e-e)/this.#r.offsetWidth,this.offsetX=e}fitY(t){const e=t?.margin??0;this.scale=(this.offsetHeight-e-e)/this.#r.offsetHeight,this.offsetY=e}}const b=r=>r.preventDefault(),v={passive:!1},x=typeof ontouchend<"u";class T extends E{static observedAttributes=[...super.observedAttributes,"pan-x","pan-y","pan-button","pinch-zoom"];#i(t,e,s,i){s=!!s,e!==s&&(i(s),s?this.setAttribute(t,""):this.removeAttribute(t))}#t=!1;get panX(){return this.#t}set panX(t){this.#i("pan-x",this.#t,t,e=>this.#t=e)}#e=!1;get panY(){return this.#e}set panY(t){this.#i("pan-y",this.#e,t,e=>this.#e=e)}#n=0;get panButton(){return this.#n}set panButton(t){this.#n!==t&&(this.setAttribute("pan-button",this.#n=t),t===2?this.addEventListener("contextmenu",b):this.removeEventListener("contextmenu",b))}#s=!1;get pinchZoom(){return this.#s}set pinchZoom(t){this.#i("pinch-zoom",this.#s,t,e=>this.#s=e)}attributeChangedCallback(t,e,s){t==="pan-x"?this.panX=s!==null:t==="pan-y"?this.panY=s!==null:t==="pan-button"?this.panButton=+s||0:t==="pinch-zoom"?this.pinchZoom=s!==null:super.attributeChangedCallback(t,e,s)}constructor(){if(super(),x){let t={x:0,y:0,d:0},e=[],s;const[i,o]=m(()=>{const n=d(e,f=>f.x),l=d(e,f=>f.y),p=t.d&&d(e,f=>f.d);p&&this.#s&&this._zoom(p/t.d,n,l),this.setOffset(this.#t||s?this.offsetX+n-t.x:this.offsetX,this.#e||s?this.offsetY+l-t.y:this.offsetY),e=[],t={x:n,y:l,d:p}}),h=({touches:n})=>({x:d(n,l=>l.clientX),y:d(n,l=>l.clientY),d:n.length>1?Math.hypot(n[0].clientX-n[1].clientX,n[0].clientY-n[1].clientY):0}),a=n=>{o(),e=[];const l=n.touches.length;s=this.#s&&l>1,l&&(t=h(n))},c=n=>{(n.touches.length===1?this.#t||this.#e:this.#s)&&(n.preventDefault(),e.push(h(n)),i())};this.addEventListener("touchstart",a),this.addEventListener("touchend",a),this.addEventListener("touchmove",c,v)}else{{let t=1,e,s;const[i]=m(()=>{this._zoom(t,e,s),t=1});this.addEventListener("wheel",o=>{this.#s&&o.ctrlKey&&(o.preventDefault(),t*=.99**o.deltaY,{clientX:e,clientY:s}=o,i())},v)}{let t,e,s,i;const[o]=m(()=>{this.setOffset(this.#t?this.offsetX+s-t:this.offsetX,this.#e?this.offsetY+i-e:this.offsetY),t=s,e=i}),h=n=>{n.preventDefault(),{clientX:s,clientY:i}=n,o()},a=n=>{(this.#t||this.#e)&&n.button===this.#n&&({clientX:t,clientY:e}=n,this.setPointerCapture(n.pointerId),this.addEventListener("pointermove",h))},c=()=>this.removeEventListener("pointermove",h);this.addEventListener("pointerdown",a),this.addEventListener("pointerup",c),this.addEventListener("pointercancel",c)}}}}customElements.define("gesture-frame",T);export{T as GestureFrame};
+const clamp = (x, min, max) => x < min ? min : x > max ? max : x;
+const clampZero = (x) => x < 0 ? 0 : x;
+const sumBy = (items, selector) => {
+  let sum = 0;
+  for (let i = 0; i < items.length; i++) {
+    sum += selector(items[i]);
+  }
+  return sum;
+};
+const averageBy = (items, selector) => items.length === 1 ? selector(items[0]) : sumBy(items, selector) / items.length;
+const throttle = (callback) => {
+  let handle;
+  const wrappedCallback = () => (handle = void 0, callback());
+  return [
+    () => handle ??= requestAnimationFrame(wrappedCallback),
+    () => handle !== void 0 && (cancelAnimationFrame(handle), handle = void 0)
+  ];
+};
+class ScrollableFrame extends HTMLElement {
+  static observedAttributes = ["scale", "min-scale", "max-scale", "offset-x", "offset-y"];
+  #scale = 1;
+  get scale() {
+    return this.#scale;
+  }
+  set scale(scale) {
+    scale = clamp(scale, this.#minScale, this.#maxScale);
+    if (this.#scale !== scale) {
+      this.#setAttribute("scale", this.#scale = scale);
+      this.#content.style.transform = `scale(${scale})`;
+    }
+  }
+  #minScale = 0.1;
+  get minScale() {
+    return this.#minScale;
+  }
+  set minScale(minScale) {
+    if (this.#minScale !== minScale && minScale > 0) {
+      this.#setAttribute("min-scale", this.#minScale = minScale);
+      this.#scale < minScale && (this.scale = minScale);
+    }
+  }
+  #maxScale = 100;
+  get maxScale() {
+    return this.#maxScale;
+  }
+  set maxScale(maxScale) {
+    if (this.#maxScale !== maxScale && maxScale > 0) {
+      this.#setAttribute("max-scale", this.#maxScale = maxScale);
+      this.#scale > maxScale && (this.scale = maxScale);
+    }
+  }
+  #offsetX = 0;
+  get offsetX() {
+    return this.#offsetX;
+  }
+  set offsetX(offsetX) {
+    this.setOffset(offsetX, this.#offsetY);
+  }
+  #offsetY = 0;
+  get offsetY() {
+    return this.#offsetY;
+  }
+  set offsetY(offsetY) {
+    this.setOffset(this.#offsetX, offsetY);
+  }
+  #marginX = 0;
+  #marginY = 0;
+  #container;
+  #topLeft;
+  #content;
+  #disableScrollEventTemporarily;
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.innerHTML = "<div part=container><div part=top-left></div><slot part=content></slot></div>";
+    this.#container = shadowRoot.firstElementChild;
+    this.#topLeft = this.#container.firstElementChild;
+    this.#content = this.#topLeft.nextElementSibling;
+    {
+      let isScrollEventEnabled = 1;
+      const [reserveListeningScrollEvent] = throttle(() => isScrollEventEnabled = 1);
+      this.#disableScrollEventTemporarily = () => {
+        reserveListeningScrollEvent();
+        isScrollEventEnabled = 0;
+      };
+      this.addEventListener("scroll", () => {
+        if (isScrollEventEnabled) {
+          const offset = this.#getCTMs()[0].transformPoint({ x: this.#marginX - this.scrollLeft, y: this.#marginY - this.scrollTop });
+          this.#setAttribute("offset-x", this.#offsetX = offset.x);
+          this.#setAttribute("offset-y", this.#offsetY = offset.y);
+        }
+      });
+    }
+  }
+  #isAttributeChangedCallbackEnabled = 1;
+  #setAttribute(name, value) {
+    this.#isAttributeChangedCallbackEnabled = 0;
+    this.setAttribute(name, value);
+    this.#isAttributeChangedCallbackEnabled = 1;
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.#isAttributeChangedCallbackEnabled && oldValue !== newValue) {
+      this[name.replace(/-([a-z])/g, (_, $1) => $1.toUpperCase())] = +newValue;
+    }
+  }
+  connectedCallback() {
+    this.#setAttribute("scale", this.#scale);
+    this.#setAttribute("min-scale", this.#minScale);
+    this.#setAttribute("max-scale", this.#maxScale);
+  }
+  #memoizedCTMString = "";
+  #memoizedCTMs = [new DOMMatrixReadOnly(), new DOMMatrixReadOnly()];
+  #getCTMs() {
+    let ctmString = "";
+    for (let element = this; element; element = element.parentElement) {
+      const transform = getComputedStyle(element).transform;
+      transform && transform !== "none" && (ctmString = `${transform} ${ctmString}`);
+    }
+    if (this.#memoizedCTMString !== ctmString) {
+      const ctm = new DOMMatrix(ctmString);
+      ctm.e = ctm.f = 0;
+      this.#memoizedCTMString = ctmString;
+      this.#memoizedCTMs = [DOMMatrixReadOnly.fromMatrix(ctm), DOMMatrixReadOnly.fromMatrix(ctm.inverse())];
+    }
+    return this.#memoizedCTMs;
+  }
+  setOffset(offsetX, offsetY) {
+    if (this.#offsetX === offsetX && this.#offsetY === offsetY) {
+      return;
+    }
+    this.#setAttribute("offset-x", this.#offsetX = offsetX);
+    this.#setAttribute("offset-y", this.#offsetY = offsetY);
+    const { x, y } = this.#getCTMs()[1].transformPoint({ x: offsetX, y: offsetY });
+    const containerStyle = this.#container.style;
+    containerStyle.margin = `${this.#marginY = clampZero(y)}px 0 0 ${this.#marginX = clampZero(x)}px`;
+    containerStyle.width = `${x < 0 ? this.clientWidth - x : 0}px`;
+    containerStyle.height = `${y < 0 ? this.clientHeight - y : 0}px`;
+    this.#disableScrollEventTemporarily();
+    this.scrollTo(clampZero(-x), clampZero(-y));
+  }
+  _zoom(scaleRatio, originClientX, originClientY) {
+    const previousScale = this.#scale;
+    const scale = clamp(previousScale * scaleRatio, this.#minScale, this.#maxScale);
+    if (scale !== previousScale) {
+      const offsetScale = scale / previousScale - 1;
+      const topLeft = this.#topLeft.getBoundingClientRect();
+      this.scale = scale;
+      this.setOffset(this.#offsetX + offsetScale * (topLeft.x - originClientX), this.#offsetY + offsetScale * (topLeft.y - originClientY));
+    }
+  }
+  /**
+   * Zoom keeping the apparent position of `(origin.x, origin.y)`. Zoom in when `scaleRatio > 1` and zoom out when `scaleRatio < 1`. `origin.x` and `origin.y` can be specified as a `number` (px) or a `` `${number}%` ``. The default value for both is `"50%"` (center).
+   */
+  zoom(scaleRatio, origin) {
+    const rect = this.getBoundingClientRect();
+    const x = origin?.x;
+    const y = origin?.y;
+    this._zoom(
+      scaleRatio,
+      rect.x + (x === void 0 ? rect.width * 0.5 : typeof x === "number" ? x : rect.width * parseFloat(x) * 0.01),
+      rect.y + (y === void 0 ? rect.height * 0.5 : typeof y === "number" ? y : rect.height * parseFloat(y) * 0.01)
+    );
+  }
+  /**
+   * Adjust the scale and offset to display the entire content.
+   */
+  fit(options) {
+    const { offsetWidth: width, offsetHeight: height } = this;
+    const { offsetWidth: contentWidth, offsetHeight: contentHeight } = this.#content;
+    const marginX = options?.marginX ?? 0;
+    const marginY = options?.marginY ?? 0;
+    const widthBasedScale = (width - marginX - marginX) / contentWidth;
+    const heightBasedScale = (height - marginY - marginY) / contentHeight;
+    if (widthBasedScale < heightBasedScale) {
+      this.scale = widthBasedScale;
+      this.setOffset(marginX, Math.floor(height - contentHeight * widthBasedScale) / 2);
+    } else {
+      this.scale = heightBasedScale;
+      this.setOffset(Math.floor(width - contentWidth * heightBasedScale) / 2, marginY);
+    }
+  }
+  /**
+   * Adjust the scale and offset-x to fit the width.
+   */
+  fitX(options) {
+    const margin = options?.margin ?? 0;
+    this.scale = (this.offsetWidth - margin - margin) / this.#content.offsetWidth;
+    this.offsetX = margin;
+  }
+  /**
+   * Adjust the scale and offset-y to fit the height.
+   */
+  fitY(options) {
+    const margin = options?.margin ?? 0;
+    this.scale = (this.offsetHeight - margin - margin) / this.#content.offsetHeight;
+    this.offsetY = margin;
+  }
+}
+const preventDefault = (event) => event.preventDefault();
+const nonPassive = { passive: false };
+const isTouchEventEnabled = typeof ontouchend !== "undefined";
+class GestureFrame extends ScrollableFrame {
+  static observedAttributes = [
+    ...super.observedAttributes,
+    "pan-x",
+    "pan-y",
+    "pan-button",
+    "pinch-zoom"
+  ];
+  #setBooleanAttribute(name, oldValue, newValue, setValue) {
+    newValue = !!newValue;
+    if (oldValue !== newValue) {
+      setValue(newValue);
+      newValue ? this.setAttribute(name, "") : this.removeAttribute(name);
+    }
+  }
+  #panX = false;
+  get panX() {
+    return this.#panX;
+  }
+  set panX(panX) {
+    this.#setBooleanAttribute("pan-x", this.#panX, panX, (panX2) => this.#panX = panX2);
+  }
+  #panY = false;
+  get panY() {
+    return this.#panY;
+  }
+  set panY(panY) {
+    this.#setBooleanAttribute("pan-y", this.#panY, panY, (panY2) => this.#panY = panY2);
+  }
+  #panButton = 0;
+  get panButton() {
+    return this.#panButton;
+  }
+  set panButton(panButton) {
+    if (this.#panButton !== panButton) {
+      this.setAttribute("pan-button", this.#panButton = panButton);
+      if (panButton === 2) {
+        this.addEventListener("contextmenu", preventDefault);
+      } else {
+        this.removeEventListener("contextmenu", preventDefault);
+      }
+    }
+  }
+  #pinchZoom = false;
+  get pinchZoom() {
+    return this.#pinchZoom;
+  }
+  set pinchZoom(pinchZoom) {
+    this.#setBooleanAttribute("pinch-zoom", this.#pinchZoom, pinchZoom, (pinchZoom2) => this.#pinchZoom = pinchZoom2);
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "pan-x") {
+      this.panX = newValue !== null;
+    } else if (name === "pan-y") {
+      this.panY = newValue !== null;
+    } else if (name === "pan-button") {
+      this.panButton = +newValue || 0;
+    } else if (name === "pinch-zoom") {
+      this.pinchZoom = newValue !== null;
+    } else {
+      super.attributeChangedCallback(name, oldValue, newValue);
+    }
+  }
+  constructor() {
+    super();
+    if (isTouchEventEnabled) {
+      let previousPoint = { x: 0, y: 0, d: 0 };
+      let points = [];
+      let multiTouchPanning;
+      const [reservePanZoom, cancelPanZoom] = throttle(() => {
+        const x = averageBy(points, (p) => p.x);
+        const y = averageBy(points, (p) => p.y);
+        const d = previousPoint.d && averageBy(points, (p) => p.d);
+        d && this.#pinchZoom && this._zoom(d / previousPoint.d, x, y);
+        this.setOffset(
+          this.#panX || multiTouchPanning ? this.offsetX + x - previousPoint.x : this.offsetX,
+          this.#panY || multiTouchPanning ? this.offsetY + y - previousPoint.y : this.offsetY
+        );
+        points = [];
+        previousPoint = { x, y, d };
+      });
+      const calculatePoint = ({ touches }) => ({
+        x: averageBy(touches, (touch) => touch.clientX),
+        y: averageBy(touches, (touch) => touch.clientY),
+        d: touches.length > 1 ? Math.hypot(touches[0].clientX - touches[1].clientX, touches[0].clientY - touches[1].clientY) : 0
+      });
+      const onTouchStartEnd = (event) => {
+        cancelPanZoom();
+        points = [];
+        const touchesLength = event.touches.length;
+        multiTouchPanning = this.#pinchZoom && touchesLength > 1;
+        touchesLength && (previousPoint = calculatePoint(event));
+      };
+      const onTouchMove = (event) => {
+        if (event.touches.length === 1 ? this.#panX || this.#panY : this.#pinchZoom) {
+          event.preventDefault();
+          points.push(calculatePoint(event));
+          reservePanZoom();
+        }
+      };
+      this.addEventListener("touchstart", onTouchStartEnd);
+      this.addEventListener("touchend", onTouchStartEnd);
+      this.addEventListener("touchmove", onTouchMove, nonPassive);
+    } else {
+      {
+        let scaleRatio = 1;
+        let clientX;
+        let clientY;
+        const [reserveZooming] = throttle(() => {
+          this._zoom(scaleRatio, clientX, clientY);
+          scaleRatio = 1;
+        });
+        this.addEventListener(
+          "wheel",
+          (event) => {
+            if (this.#pinchZoom && event.ctrlKey) {
+              event.preventDefault();
+              scaleRatio *= 0.99 ** event.deltaY;
+              ({ clientX, clientY } = event);
+              reserveZooming();
+            }
+          },
+          nonPassive
+        );
+      }
+      {
+        let previousClientX;
+        let previousClientY;
+        let clientX;
+        let clientY;
+        const [reservePanning] = throttle(() => {
+          this.setOffset(
+            this.#panX ? this.offsetX + clientX - previousClientX : this.offsetX,
+            this.#panY ? this.offsetY + clientY - previousClientY : this.offsetY
+          );
+          previousClientX = clientX;
+          previousClientY = clientY;
+        });
+        const onPointerMove = (event) => {
+          event.preventDefault();
+          ({ clientX, clientY } = event);
+          reservePanning();
+        };
+        const onPointerDown = (event) => {
+          if ((this.#panX || this.#panY) && event.button === this.#panButton) {
+            ({ clientX: previousClientX, clientY: previousClientY } = event);
+            this.setPointerCapture(event.pointerId);
+            this.addEventListener("pointermove", onPointerMove);
+          }
+        };
+        const onPointerUp = () => this.removeEventListener("pointermove", onPointerMove);
+        this.addEventListener("pointerdown", onPointerDown);
+        this.addEventListener("pointerup", onPointerUp);
+        this.addEventListener("pointercancel", onPointerUp);
+      }
+    }
+  }
+}
+customElements.define("gesture-frame", GestureFrame);
+export {
+  GestureFrame
+};
